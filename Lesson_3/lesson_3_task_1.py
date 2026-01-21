663c03f12899762380196b6f0f0f17dcadaf797d
edpetrov <dsk648400411@yandex.ru>
2026-01-21 14:53:21 +0300

    Добавил новые тесты и страницы

--- Lesson_3/lesson_3_task_1.py
+++
@@ -1,6 +0,0 @@
-from user import User
-
-my_user = User ("Ivan", "Petrov", "Ivan Petrov")
-my_user.sayFirstName()
-my_user.sayLastName()
-my_user.sayfull_name()
--- Lesson_3/Уроки/Userrrrr.py
+++
@@ -1,7 +0,0 @@
-from ClassUser import User
-from card import Card
-
-Egor = User("Egor", 31)
-
-Egor.sayName()
-Egor.sayAge()
