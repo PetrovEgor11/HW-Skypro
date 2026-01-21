480211bd55a6d58ce8a138a7822aa7a9ae92ec54
edpetrov <dsk648400411@yandex.ru>
2026-01-21 16:57:25 +0300

    Добавил все папки и файлы

--- Lesson/Lesson_1/lesson_1_task_4.py
+++
@@ -1,3 +0,0 @@
-first_name = input("Укажите ваше имя:")
-Last_name = input("Укажите вашу фамилию:")
-print("Вас зовут:" + first_name + " " + Last_name)
--- Lesson/Lesson_2/lesson_2_task_8.py
+++
@@ -1,6 +0,0 @@
-stl = [18, 14, 10, 6, 2]
-
-for i in range(18, 0, -4):
-    print(i, end=' ')
-
-
